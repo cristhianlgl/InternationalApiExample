7bc23414bbefb56cf6ce8f93cd9b51c380b5e583
cristhianlgl <cristhianlgl@hotmail.com>
2022-09-21 10:49:21 -0500

    Configurar program para trabajar con localizacion

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// 1 Localization
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+// 2. Supported Cultures
+var supportedCultures = new[] { "en_US", "es_ES", "fr_FR" };
+var localizationOptions = new RequestLocalizationOptions()
+                           .SetDefaultCulture(supportedCultures[0])
+                           .AddSupportedCultures(supportedCultures)
+                           .AddSupportedUICultures(supportedCultures);
+
+// 3. add Localization to app
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
