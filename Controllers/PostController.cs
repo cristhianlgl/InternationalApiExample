9fcc537d576d9508903a16ea58049b07ee9583b6
cristhianlgl <cristhianlgl@hotmail.com>
2022-09-21 11:23:42 -0500

    Agregar post Controller y la entidad sharedResource para probar localizacion

---
+++ Controllers/PostController.cs
@@ -0,0 +1,54 @@
+﻿using InternationalApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace InternationalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IStringLocalizer<PostController> _stringLocalizer;
+        private readonly IStringLocalizer<SharedResource> _sharedStringLocalizer;
+
+        public PostController(IStringLocalizer<PostController> stringLocalizer, IStringLocalizer<SharedResource> sharedStringLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+            _sharedStringLocalizer = sharedStringLocalizer;
+        }
+
+        [HttpGet]
+        [Route("post_resource")]
+        public IActionResult GetUsingPostControllerResource()
+        {
+            // find text
+            var article = _stringLocalizer["article"];
+            var postName = _stringLocalizer.GetString("welcome").Value ?? string.Empty;
+
+            return Ok(new
+            {
+                PostType = article,
+                PostNAme = postName
+            });
+        }
+
+        [HttpGet]
+        [Route("shared_resource")]
+        public IActionResult GetUsingSharedResource()
+        {
+            // find text
+            var article = _stringLocalizer.GetString("article");
+            var postName = _stringLocalizer.GetString("welcome").Value ?? string.Empty;
+            var todayIs = string.Format(_sharedStringLocalizer.GetString("todayIs"),
+                                DateTime.Today.ToLongDateString()); 
+
+            return Ok(new
+            {
+                PostType = article,
+                PostNAme = postName,
+                TodayIs = todayIs
+            });
+        }
+    }
+}
